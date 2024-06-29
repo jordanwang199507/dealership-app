ee0a6ec936c522d4df752c7098b3917f44f09ad0
Jordan Wang <>
2024-06-28 23:43:07 -0700

    added spline and set up sass structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Spline from "@splinetool/react-spline";
+// import "./App.css";
+import "./sass/main.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Spline
+        className="spline"
+        scene="https://prod.spline.design/9F4SYC1uL6oW9pcT/scene.splinecode"
+      />
     </div>
   );
 }
